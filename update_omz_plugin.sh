12d304957eabdad92105b8a997ff88ead76f7bd3
Zizheng Wu <me@zizhengwu.com>
2022-07-09 22:50:41 +0800

    Update omz

--- update_omz_plugin.sh
+++ update_omz_plugin.sh
@@ -5,3 +5,5 @@ ZTHEMEDIR=$HOME/.oh-my-zsh/custom/themes
 if cd $ZPLUGINDIR/fast-syntax-highlighting; then git pull; else git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git $ZPLUGINDIR/fast-syntax-highlighting; fi
 
 if cd $ZTHEMEDIR/powerlevel10k; then git pull; else git clone https://github.com/romkatv/powerlevel10k.git $ZTHEMEDIR/powerlevel10k; fi
+
+zsh -ic "omz update"

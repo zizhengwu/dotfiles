cef381d502c629b8a3985f5aa44a68b56769c856
Zizheng Wu <me@zizhengwu.com>
2023-01-06 12:34:50 +0800

    Break long 1-liner

--- update_omz_plugin.sh
+++ update_omz_plugin.sh
@@ -2,9 +2,17 @@
 ZPLUGINDIR=$HOME/.oh-my-zsh/custom/plugins
 ZTHEMEDIR=$HOME/.oh-my-zsh/custom/themes
 
-if cd $ZPLUGINDIR/fast-syntax-highlighting; then git pull; else git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git $ZPLUGINDIR/fast-syntax-highlighting; fi
+if cd $ZPLUGINDIR/fast-syntax-highlighting; then
+    git pull
+else
+    git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git $ZPLUGINDIR/fast-syntax-highlighting
+fi
 
-if cd $ZTHEMEDIR/powerlevel10k; then git pull; else git clone https://github.com/romkatv/powerlevel10k.git $ZTHEMEDIR/powerlevel10k; fi
+if cd $ZTHEMEDIR/powerlevel10k; then
+    git pull
+else
+    git clone https://github.com/romkatv/powerlevel10k.git $ZTHEMEDIR/powerlevel10k
+fi
 
 if cd $ZPLUGINDIR/F-Sy-H; then git pull; else git clone https://github.com/z-shell/F-Sy-H.git $ZPLUGINDIR/F-Sy-H; fi
 

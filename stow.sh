8339f84a986572a4b95b2ef8828f765d1b12890a
Zizheng Wu <zizhengwu@google.com>
2023-01-10 11:40:47 +0800

    Add blazerc

--- stow.sh
+++ stow.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DOTFILES_PATH="$HOME/git/dotfiles/" 
-SHARED="tmux mpv fig"
+SHARED="tmux mpv fig blaze"
 for target in $SHARED; do
-    (cd $DOTFILES_PATH; stow --target=$HOME $target)
+    (cd $DOTFILES_PATH; stow --adopt --target=$HOME $target)
 done

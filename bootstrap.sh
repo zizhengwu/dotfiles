46aa3ef5b0258772925f4bccbb04fb80b40970fb
Zizheng Wu <zizhengwu@google.com>
2023-03-08 04:02:33 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,7 @@ sudo apt install -y git
 sudo apt install -y wget
 sudo apt install -y python3
 sudo apt install -y stow
+sudo apt install -y fzf
 sudo apt install -y google-cloud-sdk
 sudo apt install -y code visual-studio-code-google3
 sudo apt install -y git-remote-google

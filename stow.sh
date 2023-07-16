c9b88e0cac5cc6b7a0ba880c8124d60bc3b51b95
Zizheng Wu <me@zizhengwu.com>
2023-07-15 20:56:14 -0700

    Only symlink files, not directories
    https://superuser.com/questions/1632928/gnu-stow-only-symlink-files-not-directories

--- stow.sh
+++ stow.sh
@@ -2,5 +2,5 @@
 DOTFILES_PATH="$HOME/git/dotfiles/" 
 SHARED="tmux mpv fig blaze bin lvim"
 for target in $SHARED; do
-    (cd $DOTFILES_PATH; stow --adopt --target=$HOME $target)
+    (cd $DOTFILES_PATH; stow --adopt --no-folding --target=$HOME $target)
 done

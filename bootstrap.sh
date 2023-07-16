d31e8d482522e9041bbd56bb2cd82b09c8ee7c66
Zizheng Wu <me@zizhengwu.com>
2023-07-15 20:59:17 -0700

    Install neovim

--- bootstrap.sh
+++ bootstrap.sh
@@ -52,7 +52,7 @@ EOF
 sudo service docker start
 sudo addgroup docker || true
 sudo adduser $USER docker
-~/.linuxbrew/bin/brew install kind k9s
+~/.linuxbrew/bin/brew install kind k9s neovim
 /google/data/ro/teams/hi/install_hi.sh
 cat <<EOF | sudo tee /etc/sudoers.d/nopasswd
 zizhengwu ALL=(ALL:ALL) NOPASSWD:ALL

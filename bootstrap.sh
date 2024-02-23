d1bba247aa293c7f3be043ab0de2d88f85020c9f
Zizheng Wu <zizhengwu@google.com>
2024-02-23 10:32:05 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -22,7 +22,6 @@ zizhengwu ALL=(ALL:ALL) NOPASSWD:ALL
 EOF
 
 # Require interactions
-sudo glinux-config set backups_exclude_dot_git no
 sudo glinux-config set custom_etc_sudoers_d true
 sudo glinux-config set backups_exclude_dot_git no
 /google/data/ro/teams/hi/install_hi.sh

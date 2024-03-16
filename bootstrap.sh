40c8648d329214bdefa64973ff6ab7fd16ea75cf
Zizheng Wu <zizhengwu@google.com>
2024-03-16 10:59:41 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,8 @@ gcert
 sudo glinux-add-repo -b docker-ce-"$(lsb_release -cs)"
 sudo glinux-add-repo -b typescript stable
 sudo apt update
-sudo apt -y install kubectl zsh git intellij-ue-beta wget python3 stow fzf google-cloud-sdk code visual-studio-code-google3 git-remote-google google-cloud-sdk-anthos-auth libvirt-daemon qemu-kvm virt-manager virt-viewer bridge-utils google-cloud-sdk-gke-gcloud-auth-plugin
+sudo apt -y install build-essential zsh git wget python3 stow fzf
+sudo apt -y install kubectl intellij-ue-beta google-cloud-sdk code visual-studio-code-google3 git-remote-google google-cloud-sdk-anthos-auth google-cloud-sdk-gke-gcloud-auth-plugin libvirt-daemon qemu-kvm virt-manager virt-viewer bridge-utils
 git clone --recurse-submodules https://github.com/zizhengwu/dotfiles.git $HOME/git/dotfiles
 wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 yes n | sh install.sh

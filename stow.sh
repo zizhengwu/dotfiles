d3251b5c92766172f4d0b884369eccee4d46f77b
Zizheng Wu <me@zizhengwu.com>
2024-05-22 12:21:57 -0700

    Add kitty

--- stow.sh
+++ stow.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DOTFILES_PATH="$HOME/git/dotfiles/" 
-SHARED="tmux mpv fig blaze bin lvim zsh"
+SHARED="tmux mpv fig blaze bin lvim zsh kitty"
 for target in $SHARED; do
     (cd $DOTFILES_PATH; stow --adopt --no-folding --target=$HOME $target)
 done

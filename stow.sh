bde4f9f4a30099079e13b36c3c4605dc868cf2f3
Zizheng Wu <me@zizhengwu.com>
2024-07-08 00:19:00 -0700

    Init astronvim

--- stow.sh
+++ stow.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DOTFILES_PATH="$HOME/git/dotfiles/" 
-SHARED="tmux mpv fig blaze bin lvim zsh kitty ssh"
+SHARED="tmux mpv fig blaze bin lvim zsh kitty ssh astronvim"
 for target in $SHARED; do
     (cd $DOTFILES_PATH; stow --adopt --no-folding --target=$HOME $target)
 done

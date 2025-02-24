576c064e15fe7cfc92165c1472c23ef92f8a7d8d
Zizheng Wu <zizhengwu@google.com>
2025-02-24 11:55:07 -0800

    Add goobuntu_backups

--- stow.sh
+++ stow.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DOTFILES_PATH="$HOME/git/dotfiles/" 
-SHARED="tmux mpv fig blaze bin lvim zsh kitty ssh astronvim"
+SHARED="tmux mpv fig blaze bin lvim zsh kitty ssh astronvim goobuntu_backups"
 for target in $SHARED; do
     (cd $DOTFILES_PATH; stow --adopt --no-folding --target=$HOME $target)
 done

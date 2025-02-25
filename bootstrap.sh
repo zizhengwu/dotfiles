20af9e164fa129289a0ca4f473e14f112adbaa4d
Zizheng Wu <zizhengwu@google.com>
2025-02-24 19:23:51 -0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,7 +16,7 @@ EOF
 git clone https://github.com/Homebrew/brew $HOME/.linuxbrew/Homebrew
 mkdir $HOME/.linuxbrew/bin
 ln -s $HOME/.linuxbrew/Homebrew/bin/brew $HOME/.linuxbrew/bin
-~/.linuxbrew/bin/brew install kind k9s neovim ripgrep kubectl
+~/.linuxbrew/bin/brew install kind k9s neovim ripgrep kubectl zoxide
 
 # Require interactions
 sudo glinux-config -UX set timezone America/Los_Angeles
